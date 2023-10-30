a0295fe833173cca85ff325cc3ec41272695451a
Wesley Alves Rolim <walvesrolim@gmail.com>
2023-10-29 23:54:33 -0300

    question 4 done

---
+++ question 4/script.js
@@ -0,0 +1,12 @@
+async function listImages(url) {
+  const response = await fetch(url);
+  const json = await response.json();
+
+  if (!Array.isArray(json)) return null;
+
+  return json;
+}
+
+listImages("https://jsonplaceholder.typicode.com/photos").then((value) => {
+  console.log(value);
+});
